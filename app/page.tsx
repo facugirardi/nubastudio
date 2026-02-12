009af8db984b5679fe90bf757c0904208783f61c
Facundo Girardi <facugirardi22@gmail.com>
2026-02-12 20:17:04 -0300

    changes

--- app/page.tsx
+++ app/page.tsx
@@ -361,7 +361,7 @@ export default function Home() {
               transition={{ duration: 0.8, delay: 0.3, ease: [0.22, 1, 0.36, 1] }}
               className="mt-2 sm:mt-3 md:mt-auto mb-20 sm:mb-24 md:mb-0 pb-0 md:pb-16 max-w-3xl"
             >
-              <p className="mt-6 sm:mt-8 md:mt-12 lg:mt-16 2xl:mt-20 text-sm sm:text-base md:text-lg lg:text-xl 2xl:text-2xl text-black leading-relaxed uppercase tracking-[0.08em]">
+              <p className="mt-6 sm:mt-8 2xl:mt-20 text-sm sm:text-base md:text-lg lg:text-xl 2xl:text-2xl text-black leading-relaxed uppercase tracking-[0.08em]">
                 WE CREATE{" "}
                 <span className="relative inline-block">
                   SOLUTIONS
